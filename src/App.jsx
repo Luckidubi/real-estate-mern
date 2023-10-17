dad46b61ba29bece56254f9b152faea1736a673e
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-10-17 18:09:55 +0400

    chore: Create routes, pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 function App() {
   return (
-    <div className="text-orange-300">App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+
+      </Routes>
+    </Router>
   )
 }
 
