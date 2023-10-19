f225e61c813c3441f73c0df4d90015b8930a8836
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-10-19 19:52:58 +0400

    feat: Add delete user feature

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { updateUser } from '../controllers/userController.js';
+import { deleteUser, updateUser } from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -7,5 +7,6 @@ const router = express.Router();
 
 
 router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
 
 export default router;
