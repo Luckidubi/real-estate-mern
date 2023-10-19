f87b140eb414a7cb3cfafa60ed49ce3d8c95000c
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-10-19 18:14:29 +0400

    chore: Create update user api route

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import authRouter from "./routes/authRoute.js"
+import userRouter from "./routes/userRoute.js"
+import cookieParser from 'cookie-parser'
 dotenv.config()
 const app = express()
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("MongoDB is Connected")
@@ -18,6 +22,7 @@ app.listen(3000, ()=>{
     console.log('Server is running on port 3000')
 })
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
