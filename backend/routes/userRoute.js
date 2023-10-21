85a7d711e30aa2ff8486dcdf4fadfb7c157a562f
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-10-22 01:27:32 +0400

    feat: Add contact owner form to listing page

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, getUserListings, updateUser } from '../controllers/userController.js';
+import { deleteUser, getUser, getUserListings, updateUser } from '../controllers/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import { useSelector } from 'react-redux';
 import {
     FaBath,
     FaBed,
@@ -13,6 +14,7 @@ import {
     FaParking,
     FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 export default function Listing() {
     SwiperCore.use([Navigation]);
@@ -21,6 +23,9 @@ export default function Listing() {
     const [copied, setCopied] = useState(false);
     const [error, setError] = useState(false);
     const params = useParams();
+    const { currentUser } = useSelector((state) => state.user);
+    const [contact, setContact] = useState(false);
+
     useEffect(() => {
         const fetchListing = async () => {
             try {
@@ -42,7 +47,7 @@ export default function Listing() {
         };
         fetchListing();
     }, [params.listingId]);
-    console.log(loading);
+
 
     return (
         <main>
@@ -130,6 +135,12 @@ export default function Listing() {
                                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
                             </li>
                         </ul>
+                        {currentUser && listing.userRef !== currentUser._id && !contact && (
+                            <button onClick={() => setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                                Contact landlord
+                            </button>
+                        )}
+                        {contact && <Contact listing={listing} />}
                     </div>
                 </div>
             )}
