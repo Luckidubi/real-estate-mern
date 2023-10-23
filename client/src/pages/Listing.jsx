45ecff6de73da2b129ed6f946e2b6a2589fefcc5
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-10-23 22:20:55 +0400

    feat: Update Home page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -136,7 +136,7 @@ export default function Listing() {
                             </li>
                         </ul>
                         {currentUser && listing.userRef !== currentUser._id && !contact && (
-                            <button onClick={() => setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+                            <button onClick={() => setContact(true)} className='bg-sky-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
                                 Contact landlord
                             </button>
                         )}
