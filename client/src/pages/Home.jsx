968c86dfbdf789f627a75a448f6c352a541600ce
Dubem Nwodo <nwodochidubem1@gmail.com>
2023-10-24 00:07:48 +0400

    chore: Update home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -51,15 +51,14 @@ function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
-          <br />
-          place with ease
+          Unlock Your <span className='text-slate-500'> Dream </span> Home  <br />
+          at Lucki Estate
+
+
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
-          live.
-          <br />
-          We have a wide range of properties for you to choose from.
+
+          Looking for that perfect place to call your own? You're in the right spot!  <br /> From cozy cabins to sky-high penthouses, we've got it all. 
         </div>
         <Link
           to={'/search'}
